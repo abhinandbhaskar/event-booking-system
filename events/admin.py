1f5d6fedbc89170c182563c777087dce0dd53e88
Abhinand Bhaskar <abhinandbhaskar43@gmail.com>
2025-07-15 16:10:36 +0530

    Add admin registration for CustomUser, Events, and Booking

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from events.models import CustomUser,Events,Booking
+
+admin.site.register(CustomUser)
+admin.site.register(Events)
+admin.site.register(Booking)
