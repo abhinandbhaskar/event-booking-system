e3e207061c66887c9de0bad222ca6fbf6352ce38
Abhinand Bhaskar <abhinandbhaskar43@gmail.com>
2025-07-15 16:36:53 +0530

    fix(admin): ensure password hashing for CustomUser in admin interface

--- events/admin.py
+++ events/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.hashers import make_password
+from events.models import CustomUser, Events, Booking
 
-from events.models import CustomUser,Events,Booking
+class CustomUserAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if 'password' in form.changed_data and not change:
+            obj.password = make_password(form.cleaned_data['password'])
+        super().save_model(request, obj, form, change)
 
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Events)
 admin.site.register(Booking)
